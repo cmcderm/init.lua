cf96581e8546c54c1e6ca854e01be784e9fe933d
Connor McDermott <connor.mcdermott@walgreens.com>
2025-09-03 14:18:02 -0500

    Add formatter to JS/TS to be prettier

--- lua/theprimeagen/lazy/conform.lua
+++ lua/theprimeagen/lazy/conform.lua
@@ -5,7 +5,9 @@ return {
         require("conform").setup({
             formatters_by_ft = {
                 lua = { "stylua" },
-                go = { "gofmt" }
+                go = { "gofmt" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
             }
         })
     end
